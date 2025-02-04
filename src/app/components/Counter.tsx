dbbeca03bd4b878a3839747dc26f84caceb52a57
Jo <johj703@nate.com>
2025-02-04 17:46:28 +0900

    fix: Counter 컴포넌트에 감소 버튼 추가

--- src/app/components/Counter.tsx
+++ src/app/components/Counter.tsx
@@ -6,8 +6,14 @@ export const Counter = () => {
   const { state, dispatch } = useAppContext();
   return (
     <div>
-      <h2>카운터: {state.count}</h2>
-      <button onClick={() => dispatch({ type: "INCREMENT" })}>증가</button>
+      <div>
+        <h2>카운터</h2>
+        <p>{state.count}</p>
+      </div>
+      <div>
+        <button onClick={() => dispatch({ type: "DECREMENT" })}>감소</button>
+        <button onClick={() => dispatch({ type: "INCREMENT" })}>증가</button>
+      </div>
     </div>
   );
 };
