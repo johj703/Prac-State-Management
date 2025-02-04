368f0c6ed66ddc39dd3a1fa74896e8fe5d33dde1
Jo <johj703@nate.com>
2025-02-04 17:07:50 +0900

    add: AppContext 페이지 생성

---
+++ src/app/context/AppContext.tsx
@@ -0,0 +1,40 @@
+import React, { createContext, useReducer } from "react";
+
+interface State {
+  count: number;
+  user: string | null;
+}
+
+type Action = { type: "INCREMENT" } | { type: "SET_USER"; payload: string };
+
+const initialState: State = {
+  count: 0,
+  user: null,
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { ...state, count: state.count + 1 };
+    case "SET_USER":
+      return { ...state, user: action.payload };
+    default:
+      return state;
+  }
+};
+
+const AppContext = createContext<
+  | {
+      state: State;
+      dispatch: React.Dispatch<Action>;
+    }
+  | undefined
+>(undefined);
+export const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
